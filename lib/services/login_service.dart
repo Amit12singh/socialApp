66b1545c055ab1c2d64e51baf53c1df7b989e19a
sagar <rsaagar7200@gmail.com>
2023-10-12 16:36:09 +0545

    add:login successfull

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -4,11 +4,12 @@ import 'package:myapp/utilities/localstorage.dart';
 
 class GraphQLService {
   static GraphQLConfig graphQLConfig = GraphQLConfig();
-  static HandleToken handleTokenService = HandleToken();
+  static HandleToken handleTokenService = new HandleToken();
   GraphQLClient client = graphQLConfig.clientToQuery();
 
 
   Future<bool> login({required String email, required String password}) async {
+    print(email);
     try {
       QueryResult result = await client.mutate(
         MutationOptions(
@@ -32,17 +33,26 @@ class GraphQLService {
         ),
       );
       if (result.hasException) {
+        print(result.exception);
 
         throw Exception(result.exception);
       }
 
-      Map? res = result?.data?['login'];
+     
+      Map? res = result?.data?["login"];
 
-      final isTokenSaved =
+      final isSaved =
           await handleTokenService.saveAccessToken(res?['accessToken']);
 
-      print(handleTokenService.getAccessToken());
+
+      if (isSaved) {
+        final token = await handleTokenService.getAccessToken();
+        print('token is $token');
         return true;
+      } else {
+        return false;
+      }
+     
       
     } catch (error) {
       return false;
--- lib/utilities/localstorage.dart
+++ lib/utilities/localstorage.dart
@@ -3,14 +3,23 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class HandleToken {
   // Create storage
 
-  final storage = const FlutterSecureStorage();
 
-  Future<void> saveAccessToken(String token) async {
+  Future<bool> saveAccessToken(String token) async {
+    const storage = FlutterSecureStorage();
+    print('here accessToken $token');
 // Write value
-    return await storage.write(key: 'accessToken', value: token);
+try {
+      await storage.write(key: 'accessToken', value: token);
+      return true;
+    } catch (err) {
+      return false;
+    }
+     
   }
 
   Future<String?> getAccessToken() async {
+    const storage = FlutterSecureStorage();
+
     return await storage.read(key: 'accessToken');
   }
 }
