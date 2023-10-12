3465b853f48be53abd06335007fa25934482c659
sagar <rsaagar7200@gmail.com>
2023-10-12 20:17:43 +0545

    add: auth link

--- lib/config/graphql_config.dart
+++ lib/config/graphql_config.dart
@@ -3,30 +3,20 @@ import 'package:graphql/client.dart';
 import 'package:myapp/utilities/localstorage.dart';
 
 class GraphQLConfig {
+  static HandleToken handleTokenService = HandleToken();
+  
 
-  HandleToken? tokenService;
-  String? token;
-
-  GraphQLConfig() {
-    tokenService = HandleToken();
-    token = '';
-  }
-
-  Future<String?> initializeToken() async {
-    return await tokenService?.getAccessToken();
-  }
-
+  final token = handleTokenService.getAccessToken();
 
   final HttpLink httpLink = HttpLink(
     "https://apis.oldnabhaite.site/oldnabhaiteapis",
   );
 
 
-
-  // final AuthLink authLink = AuthLink(getToken: () async => 'BEARER $initializeToken()');
+  final AuthLink authLink = AuthLink(getToken: () async => 'BEARER \$token');
 
   GraphQLClient clientToQuery() => GraphQLClient(
         cache: GraphQLCache(),
-        link: httpLink,
+        link: authLink.concat(httpLink),
       );
 }
